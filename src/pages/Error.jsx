bc5d8f01b5946ce560aa764b0d43c6869c2600b8
mwafrika <mwafrikajosue@gmail.com>
2022-10-27 08:53:50 +0200

    Error page frontend done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="Image error" />
+        <h3>Ohh! page not found</h3>
+        <p>We can't seem to find the page you are looking for</p>
+        <Link to="/">Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
