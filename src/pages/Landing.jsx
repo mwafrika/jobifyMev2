1a53acd317d219ea6fca2fc5d3e2875eeb30f5d4
mwafrika <mwafrikajosue@gmail.com>
2022-10-27 07:21:49 +0200

    Finish landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,30 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span>app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+            sapiente accusantium sint nemo quibusdam voluptates asperiores,
+            voluptate, voluptatibus aperiam qui soluta. Rerum ad temporibus
+            optio dolorum voluptas suscipit, quibusdam perferendis.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
