223a7acaf1c73f55e5063cb2c3c13887c73381cc
mwafrika <mwafrikajosue@gmail.com>
2022-10-28 17:02:35 +0200

    Add register user schema

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,18 @@
-const register = (req, res) => {
-  res.send({
-    message: "register success",
-  });
+import User from "../models/User.js";
+
+const register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
 };
 
 const login = (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
   name: {
