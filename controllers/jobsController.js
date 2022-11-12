51f3f634b75b88a041f05879f0d7d22a2bb23802
mwafrika <mwafrikajosue@gmail.com>
2022-11-12 22:52:26 +0200

    monthly application done

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -73,7 +73,20 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   };
 
-  let monthlyApplications = [];
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          month: { $month: "$createdAt" },
+          year: { $year: "$createdAt" },
+          count: { $sum: 1 },
+        },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
 
   res.status(StatusCodes.OK).json({ defaultValues, monthlyApplications });
 };
