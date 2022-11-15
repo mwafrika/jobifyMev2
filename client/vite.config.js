617cce471e72de727a3121e13180c93b5df55093
mwafrika <mwafrikajosue@gmail.com>
2022-11-15 12:54:58 +0200

    suppress warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: "", // proxy to backend server
+    // proxy: "",  proxy to backend server
   },
   plugins: [react()],
   build: {
