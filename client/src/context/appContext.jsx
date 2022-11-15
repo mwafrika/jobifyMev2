fb9764c29bc4e5460c947948bee7560639ea2470
mwafrika <mwafrikajosue@gmail.com>
2022-11-15 12:38:30 +0200

    suppress warning

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -67,12 +67,10 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const url =
-    process.env.NODE_ENV === "production"
-      ? "https://huntify.onrender.com"
-      : "http://localhost:7000";
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:7000/";
 
   const authFetch = axios.create({
-    baseURL: `${url}/api/v1/`,
+    baseURL: `${url}api/v1/`,
     headers: {
       Authorization: `Bearer ${state.token}`,
     },
