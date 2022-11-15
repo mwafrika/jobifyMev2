8187bd973aca732baf5b3ee3a4e6808c3ea5ab96
mwafrika <mwafrikajosue@gmail.com>
2022-11-15 10:00:31 +0200

    Fix deployment on render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,24 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      // '/api': 'Enter your backend host with port',
+    },
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-styled-components",
+            {
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
 });
