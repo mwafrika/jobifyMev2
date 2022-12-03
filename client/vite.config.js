744711a829429020a37c10318a998748f9880ac9
mwafrika <mwafrikajosue@gmail.com>
2022-12-03 21:16:12 +0200

    Configure vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,8 +10,15 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "react-is"],
+      output: {
+        //added output object
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react-is": "ReactIs",
+        },
+      },
     },
     chunkSizeWarningLimit: 1600, // suppress warning
   },
-
 });
