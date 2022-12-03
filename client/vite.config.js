67a44de7b982e3087694af392d4040b18a73eb97
mwafrika <mwafrikajosue@gmail.com>
2022-12-03 21:50:51 +0200

    Fix bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import externalGlobals from "rollup-plugin-external-globals";
 
-//import reactRefresh from '@vitejs/plugin-react-refresh'
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    // proxy: "",  proxy to backend server
+  plugins: [
+    react(),
+    externalGlobals({
+      react: "React",
+    }),
+  ],
+  input: ["src/index.jsx"],
+  output: {
+    dir: "dist",
+    format: "es",
   },
-  plugins: [react()],
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "react-is"],
     },
     chunkSizeWarningLimit: 1600, // suppress warning
-  },
-  resolve: {
-    alias: {
-      react: "https://unpkg.com/react@18/umd/react.production.min.js",
-      "react-dom":
-        "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
-    },
   },
 });
