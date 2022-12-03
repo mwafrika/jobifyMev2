ea9adaa0cc8240adb31bc6fc73451b3467306098
mwafrika <mwafrikajosue@gmail.com>
2022-12-03 21:59:10 +0200

    rollback the changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import externalGlobals from "rollup-plugin-external-globals";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    externalGlobals({
-      react: "React",
-    }),
-  ],
-  input: ["src/index.jsx"],
-  output: {
-    dir: "dist",
-    format: "es",
-  },
+  plugins: [react()],
+
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "react-is"],
