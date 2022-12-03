93bc729be44bd5a4938bb484f179ef50e1c00e28
mwafrika <mwafrikajosue@gmail.com>
2022-12-03 21:35:31 +0200

    Configure vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 
 //import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
