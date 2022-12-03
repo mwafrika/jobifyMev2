8b0987ca5c1c1725e78460b8b5d5b5b6e87fc5be
mwafrika <mwafrikajosue@gmail.com>
2022-12-03 18:17:01 +0200

    Fix issues in deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import { AppProvider } from "./context/appContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
