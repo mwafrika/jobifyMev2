2c0c21298081503d771a7bd779c6670879649232
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 07:29:27 +0200

    Remove unwanted config

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,15 +10,16 @@ export default defineConfig({
     // rollupOptions: {
     //   // external: ["react", "react-dom", "react-is"],
     //   external: ["react", "react-dom", "react-is", "react-router-dom"],
+    external: ["react"],
     // },
     chunkSizeWarningLimit: 1600, // suppress warning
   },
-  resolve: {
-    alias: {
-      react: "https://cdn.skypack.dev/react",
-      "react-dom": "https://cdn.skypack.dev/react-dom",
-      "react-is": "https://cdn.skypack.dev/react-is",
-      "react-router-dom": "https://cdn.skypack.dev/react-router-dom",
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     react: "https://cdn.skypack.dev/react",
+  //     "react-dom": "https://cdn.skypack.dev/react-dom",
+  //     "react-is": "https://cdn.skypack.dev/react-is",
+  //     "react-router-dom": "https://cdn.skypack.dev/react-router-dom",
+  //   },
+  // },
 });
