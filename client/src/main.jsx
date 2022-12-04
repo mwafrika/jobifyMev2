74847f1e7dcf10e7891d623d989d029afe1478b2
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 07:16:51 +0200

    Remove unwanted config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 import { AppProvider } from "./context/appContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
