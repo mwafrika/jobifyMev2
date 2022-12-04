b7344f60ae45088fcd00c515d0fa9d64a10a52f1
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 22:13:25 +0200

    Fix deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,38 +1,38 @@
-// import { defineConfig } from "vite";
-// import reactRefresh from "@vitejs/plugin-react";
-// import svgrPlugin from "vite-plugin-svgr";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   // This changes the out put dir from dist to build
-//   // comment this out if that isn't relevant for your project
-//   build: {
-//     outDir: "dist",
-//   },
-//   plugins: [
-//     reactRefresh(),
-//     svgrPlugin({
-//       svgrOptions: {
-//         icon: true,
-//         // ...svgr options (https://react-svgr.com/docs/options/)
-//       },
-//     }),
-//   ],
-// });
-
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jobifyMev2/client/",
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
   build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom", "react-is"],
-    },
+    outDir: "dist",
     chunkSizeWarningLimit: 1600, // suppress warning
   },
-  plugins: [react()],
-  // define: {
-  //   "process.env": {},
-  // },
+  plugins: [
+    reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       external: ["react", "react-dom", "react-router-dom", "react-is"],
+//     },
+//     chunkSizeWarningLimit: 1600, // suppress warning
+//   },
+//   plugins: [react()],
+//   // define: {
+//   //   "process.env": {},
+//   // },
+// });
