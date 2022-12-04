a973e5e7082a8124d2fae718263bf9aac8f9bbe5
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 06:59:26 +0200

    Remove unwanted config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-is"],
-    },
-    chunkSizeWarningLimit: 1600, // suppress warning
-  },
-  resolve: {
-    alias: {
-      react: "https://cdn.skypack.dev/react",
-      "react-dom": "https://cdn.skypack.dev/react-dom",
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["react", "react-dom", "react-is"],
+  //   },
+  //   chunkSizeWarningLimit: 1600, // suppress warning
+  // },
+  // resolve: {
+  //   alias: {
+  //     react: "https://cdn.skypack.dev/react",
+  //     "react-dom": "https://cdn.skypack.dev/react-dom",
+  //   },
+  // },
 });
