744825fdaa58feb3d7e2e52e1f21ba58b81a3e4a
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 11:50:56 +0200

    Host as full app

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     "process.env": {},
   },
   build: {
-    // rollupOptions: {
-    //   external: ["react", "react-dom", "react-is", "react-router-dom"],
-    // },
+    rollupOptions: {
+      external: ["react-is"],
+    },
     chunkSizeWarningLimit: 1600, // suppress warning
   },
   // resolve: {
