75a656dde517f2aaeec50b246040e604485548f7
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 23:22:51 +0200

    Fix the configuration bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
-import react from "@vitejs/plugin-react"; // added
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 1600, // suppress warning
-    // rollupOptions: {
-    //   external: ["react", "react-dom", "react-is"],
-    // },
   },
   plugins: [
     react({
@@ -26,28 +18,4 @@ export default defineConfig({
       },
     }),
   ],
-  // plugins: [
-  //   reactRefresh(),
-  //   svgrPlugin({
-  //     svgrOptions: {
-  //       icon: true,
-  //     },
-  //   }),
-  // ],
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   build: {
-//     rollupOptions: {
-//       external: ["react", "react-dom", "react-router-dom", "react-is"],
-//     },
-//     chunkSizeWarningLimit: 1600, // suppress warning
-//   },
-//   plugins: [react()],
-//   // define: {
-//   //   "process.env": {},
-//   // },
-// });
