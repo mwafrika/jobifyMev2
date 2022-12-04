5276837daf79ea75e6093e87f19d8745ae84b85d
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 07:05:23 +0200

    Remove unwanted config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {},
+  },
   // build: {
   //   rollupOptions: {
   //     external: ["react", "react-dom", "react-is"],
