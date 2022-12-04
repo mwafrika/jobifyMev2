f59e488aad7cb6a2988117e5b5fa8f79de17a53a
mwafrika <mwafrikajosue@gmail.com>
2022-12-04 13:24:52 +0200

    Fixx config

--- client/vite.config.js
+++ client/vite.config.js
@@ -24,7 +24,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/client/",
+  base: "./client",
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "react-router-dom", "react-is"],
