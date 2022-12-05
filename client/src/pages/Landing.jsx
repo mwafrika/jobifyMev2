29b89661e5f89b0199feb5676c2afd2b761720ba
mwafrika <mwafrikajosue@gmail.com>
2022-12-06 01:32:43 +0200

    correct the description

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -12,12 +12,6 @@ const Landing = () => {
     navigate("/register");
   };
 
-  // const images = [
-  //   "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  //   "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-  //   "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  // ];
-
   const images = [main, alternative, hunter];
 
   return (
@@ -31,7 +25,7 @@ const Landing = () => {
             job <span>tracking </span>app
           </h1>
           <p>
-            Jobify Tracks your job applications and keep your job search
+            Huntify Tracks your job applications and keep your job search
             organized, all in one place. No more spreadsheets, no more emails,
             no more lost job applications.
           </p>
@@ -54,7 +48,6 @@ const Landing = () => {
             </div>
           </Fade>
         </div>
-        {/* <img src={main} alt="" className="img main-img" /> */}
       </div>
     </Wrapper>
   );
