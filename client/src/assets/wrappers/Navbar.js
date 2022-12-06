3c59e941787948944183f9646924a8bfb5eee628
mwafrika <mwafrikajosue@gmail.com>
2022-12-06 02:10:08 +0200

    correct the description

--- client/src/assets/wrappers/Navbar.js
+++ client/src/assets/wrappers/Navbar.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Wrapper = styled.nav`
   height: var(--nav-height);
@@ -10,6 +10,7 @@ const Wrapper = styled.nav`
     display: flex;
     align-items: center;
     width: 100px;
+    /* width: 15rem; */
   }
   .nav-center {
     display: flex;
@@ -80,5 +81,5 @@ const Wrapper = styled.nav`
       display: block;
     }
   }
-`
-export default Wrapper
+`;
+export default Wrapper;
--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,7 +1,9 @@
 import logo from "../assets/images/logo.svg";
 
 const Logo = () => {
-  return <img src={logo} alt="logo" className="logo" />;
+  return (
+    <img src={logo} alt="logo" className="logo" style={{ width: "15rem" }} />
+  );
 };
 
 export default Logo;
