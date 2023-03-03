78a309813c750a2e6d0fdcf39cf83321f7e60dec
mwafrika <mwafrikajosue@gmail.com>
2023-03-03 21:03:15 +0200

    Add pagination on the server side

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -49,10 +49,15 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   const jobs = await result;
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
